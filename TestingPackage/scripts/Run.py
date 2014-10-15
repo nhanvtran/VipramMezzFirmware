ef25de7f84851e3faaae2358528a6c6c4f14ff71
sergojin <sergojin@gmail.com>
2014-10-15 10:30:46 -0500

    modifications to realistic test

--- TestingPackage/scripts/Run.py
+++ TestingPackage/scripts/Run.py
@@ -67,6 +67,7 @@ if __name__ == '__main__':
     # ------------------------------------------------
     # generate the patterns
     pattern1 = stressTest_split("tmp1",options.NStress,options.freq,options.Load);
+    #pattern1 = stressTest("tmp1",options.NStress,options.freq);
     #pattern1 = exampleTest("tmp1");
     #pattern1  = realisticTest("tmp1", options.freq, options.layers);
 
